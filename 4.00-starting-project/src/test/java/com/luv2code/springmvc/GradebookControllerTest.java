b21b5d0072943e1b7f47ba292c37e279a504d856
fgb <fgb@fgbDebian.pgonbo.com>
2023-06-04 15:09:35 -0600

    creating a valid grade, student does not exist

--- 4.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 4.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -223,6 +223,18 @@ class GradebookControllerTest {
                 .andExpect(jsonPath("$.studentGrades.mathGradeResults", hasSize(2)));
     }
 
+    @Test
+    void creatingAValidGradeHttpRequestStudentDoesNotExistEmptyResponse() throws Exception {
+        this.mockMvc.perform(post("/grades")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("grade", "85.00")
+                .param("gradeType", "math")
+                .param("studentId", "0"))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status", is(404)))
+                .andExpect(jsonPath("$.message", is("Student or Grade was not found")));          
+    }
+
     @AfterEach
     void setupAfterTransaction() {
         jdbc.execute(sqlDeleteStudent);
